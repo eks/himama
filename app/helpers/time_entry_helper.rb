3a0a4efd0152daf9a397eae92af3fee800d55b91
Eduardo Koike <edu.fox@gmail.com>
2021-06-11 02:35:29 -0300

    Add edit link helper

--- app/helpers/time_entry_helper.rb
+++ app/helpers/time_entry_helper.rb
@@ -6,4 +6,10 @@ module TimeEntryHelper
   def duration_helper(entry)
     entry.in_progress? ? '' : entry.duration
   end
+
+  def edit_link_helper(entry)
+    return '' if entry.in_progress?
+
+    link_to 'Edit this entry', edit_time_entry_path(entry)
+  end
 end
--- spec/helpers/time_entry_helper_spec.rb
+++ spec/helpers/time_entry_helper_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe TimeEntryHelper, type: :helper do
 
     context 'when entry is not in progress' do
       it 'returns formatted time string' do
-        expect(helper.end_time_helper(entry)).to eq(Time.zone.at(entry.end_at).strftime('%H:%m:%S'))
+        expect(helper.end_time_helper(entry)).to eq(Time.zone.at(entry.end_at).strftime('%H:%M:%S'))
       end
     end
   end
@@ -31,5 +31,22 @@ RSpec.describe TimeEntryHelper, type: :helper do
         expect(helper.duration_helper(entry)).to eq('4 hours')
       end
     end
+  end
+
+  describe '#edit_link_helper' do
+    context 'when entry is in progress' do
+      it 'returns an empty string' do
+        entry.end_at = entry.start_at
+        expect(helper.edit_link_helper(entry)).to eq('')
+      end
+    end
+
+    context 'when entry is not in progress' do
+      it 'returns a link to edit entry' do
+        entry.save
+        expect(helper.edit_link_helper(entry)).to eq("<a href=\"#{edit_time_entry_path(entry)}\">Edit this entry</a>")
+      end
+    end
+
   end
 end
