50ef98f7f657d83e75c49dc9667d00f8768454bb
Eduardo Koike <edu.fox@gmail.com>
2021-06-10 23:39:42 -0300

    Add the ability to create clock in and clock out entries

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -1,5 +1,33 @@
 class TimeEntriesController < ApplicationController
+  before_action :set_date, only: [:create]
+
   def index
     @entries = current_user.time_entries
+  end
+
+  def create
+    if !current_user.time_entries.last.in_progress?
+      @entry = current_user.time_entries.new
+      @entry.day = @date.strftime('%Y-%m-%d')
+      @entry.start_at = @date.to_i
+      @entry.end_at = @date.to_i
+      flash_message = 'Clock In created successfuly'
+    else
+      @entry = current_user.time_entries.last
+      @entry.end_at = @date.to_i
+      flash_message = 'Clock Out created successfuly'
+    end
+
+    if @entry.save
+      flash[:success] = flash_message
+    end
+
+    redirect_to time_entries_path
+  end
+
+  private
+
+  def set_date
+    @date = Time.zone.now
   end
 end
